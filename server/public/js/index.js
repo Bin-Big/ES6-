0bfab068fc546e447bc649a5b95e6fe920f45e74
yinguohui <yinguohui@bytedance.com>
2017-03-31 14:00:40 +0800

    map and set

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -64,59 +64,45 @@
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 	{
-	  // Set的定义
 	  var list = new Set();
 	  list.add(5);
 	  list.add(7);
 
-	  console.log('zize', list.size);
+	  console.log('size', list.size);
 	}
 
 	{
-	  // Set的定义
 	  var arr = [1, 2, 3, 4, 5];
 	  var _list = new Set(arr);
+
 	  console.log('size', _list.size);
 	}
 
 	{
-	  // Set的元素是唯一的
 	  var _list2 = new Set();
 	  _list2.add(1);
 	  _list2.add(2);
 	  _list2.add(1);
+
 	  console.log('list', _list2);
 
-	  // 去重
-	  var _arr = [1, 2, 3, 1, 2];
+	  var _arr = [1, 2, 3, 1, '2'];
 	  var list2 = new Set(_arr);
-	  console.log('list unique', list2);
 
-	  var arr2 = [1, 2, 3, 1, '2'];
-	  var list3 = new Set(arr2);
-	  console.log('不换转换数据类型', list3);
+	  console.log('unique', list2);
 	}
 
 	{
-	  // Set实例的几个方法
 	  var _arr2 = ['add', 'delete', 'clear', 'has'];
-
 	  var _list3 = new Set(_arr2);
 
 	  console.log('has', _list3.has('add'));
 	  console.log('delete', _list3.delete('add'), _list3);
-
 	  _list3.clear();
-
 	  console.log('list', _list3);
 	}
 
 	{
-	  // Set实例的读取
-	  // keys()：返回键名的遍历器
-	  // values()：返回键值的遍历器
-	  // entries()：返回键值对的遍历器
-	  // forEach()：使用回调函数遍历每个成员
 	  var _arr3 = ['add', 'delete', 'clear', 'has'];
 	  var _list4 = new Set(_arr3);
 
@@ -130,7 +116,6 @@
 
 	      console.log('keys', key);
 	    }
-	    // 也可以省略values
 	  } catch (err) {
 	    _didIteratorError = true;
 	    _iteratorError = err;
@@ -151,10 +136,10 @@
 	  var _iteratorError2 = undefined;
 
 	  try {
-	    for (var _iterator2 = _list4[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+	    for (var _iterator2 = _list4.values()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
 	      var value = _step2.value;
 
-	      console.log('values', value);
+	      console.log('value', value);
 	    }
 	  } catch (err) {
 	    _didIteratorError2 = true;
@@ -181,7 +166,7 @@
 	          _key = _step3$value[0],
 	          _value = _step3$value[1];
 
-	      console.log('entries', [_key, _value]);
+	      console.log('entries', _key, _value);
 	    }
 	  } catch (err) {
 	    _didIteratorError3 = true;
@@ -199,33 +184,24 @@
 	  }
 
 	  _list4.forEach(function (item) {
-	    return console.log('forEach', item);
+	    console.log(item);
 	  });
 	}
 
 	{
-
-	  // weakSet
 	  var weakList = new WeakSet();
+
 	  var arg = {};
 
-	  weakList.add(document);
 	  weakList.add(arg);
 
-	  console.log('has-document', weakList.has(document));
-	  console.log('has-arg', weakList.has(arg));
+	  // weakList.add(2);
 
-	  console.log('delete', weakList.delete(document), weakList);
-
-	  // 没有size属性，不能遍历，弱引用
+	  console.log('weakList', weakList);
 	}
 
 	{
-	  // JavaScript的对象（Object），
-	  // 本质上是键值对的集合（Hash结构），
-	  // 但是传统上只能用字符串当作键。这给它的使用带来了很大的限制。
 	  var map = new Map();
-
 	  var _arr4 = ['123'];
 
 	  map.set(_arr4, 456);
@@ -234,31 +210,24 @@
 	}
 
 	{
-	  // Map定义可以带参数
-	  var _map = new Map([['a', 123], ['b', 345, 'c', 456]]);
-
-	  console.log('arg', _map);
-
+	  var _map = new Map([['a', 123], ['b', 456]]);
+	  console.log('map args', _map);
 	  console.log('size', _map.size);
-	  console.log('get', _map.get('a'));
 	  console.log('delete', _map.delete('a'), _map);
 	  console.log('clear', _map.clear(), _map);
-	  // keys()：返回键名的遍历器。
-	  // values()：返回键值的遍历器。
-	  // entries()：返回所有成员的遍历器。
-	  // forEach()：遍历Map的所有成员
 	}
 
 	{
 	  var weakmap = new WeakMap();
 
 	  var o = {};
-
 	  weakmap.set(o, 123);
-	  console.log('weakmap', weakmap.get(o));
+	  console.log(weakmap.get(o));
+	}
 
-	  // weakmap.set('12','34');
-	  // 没有size属性，不能遍历，不能clear
+	{
+	  var a1 = ['<a href="#baidu.com">s</a>'];
+	  document.body.innerHTML = a1[0];
 	}
 
 /***/ }
