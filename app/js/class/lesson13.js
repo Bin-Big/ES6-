1b314f03a26d583229ef183d40d29c24537d4f78
yinguohui <yinguohui@bytedance.com>
2017-04-06 10:11:55 +0800

    修改promise重录

--- app/js/class/lesson13.js
+++ app/js/class/lesson13.js
@@ -3,17 +3,15 @@
   let ajax=function(callback){
     console.log('执行');
     setTimeout(function () {
-      callback&&callback();
+      callback&&callback.call()
     }, 1000);
   };
-
   ajax(function(){
     console.log('timeout1');
-  });
+  })
 }
 
 {
-  // promise
   let ajax=function(){
     console.log('执行2');
     return new Promise(function(resolve,reject){
@@ -21,15 +19,14 @@
         resolve()
       }, 1000);
     })
-  }
+  };
 
   ajax().then(function(){
     console.log('promise','timeout2');
-  });
+  })
 }
 
 {
-  // 多步调用
   let ajax=function(){
     console.log('执行3');
     return new Promise(function(resolve,reject){
@@ -37,39 +34,42 @@
         resolve()
       }, 1000);
     })
-  }
-  ajax().then(function(){
+  };
+
+  ajax()
+    .then(function(){
     return new Promise(function(resolve,reject){
       setTimeout(function () {
         resolve()
-      }, 1000);
-    })
-  }).then(function(){
+      }, 2000);
+    });
+  })
+    .then(function(){
     console.log('timeout3');
-  });
+  })
 }
 
 {
   let ajax=function(num){
-    console.log('执行3');
+    console.log('执行4');
     return new Promise(function(resolve,reject){
       if(num>5){
-        resolve();
+        resolve()
       }else{
-        throw new Error('出错了');
+        throw new Error('出错了')
       }
     })
   }
 
   ajax(6).then(function(){
-    console.log(6);
+    console.log('log',6);
   }).catch(function(err){
     console.log('catch',err);
-  })
+  });
 
   ajax(3).then(function(){
-    console.log(3);
+    console.log('log',3);
   }).catch(function(err){
     console.log('catch',err);
-  })
+  });
 }
