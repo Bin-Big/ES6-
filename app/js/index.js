fda1841636e33e8dc71314d9e6049f4268c2a1a7
yinguohui <yinguohui@bytedance.com>
2017-04-18 11:09:32 +0800

    最终代码

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,6 @@
-// import './class/lesson1';
-// import 'babel-polyfill';
-// import './class/lesson17';
-//
-// import {A,test,Hello} from './class/lesson17';
-// import {A} from './class/lesson17';
-// import * as lesson from './class/lesson17';
-//
-import Lesson17 from './class/lesson17';
+import 'babel-polyfill';
+import Lottery from './lottery';
 
-// console.log(A,test,Hello);
-// console.log(lesson.A,lesson.test);
-console.log(Lesson17.A);
+const syy=new Lottery();
+
+console.log(syy);
